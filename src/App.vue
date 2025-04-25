46b3ec7f8c53dab57a22314cad4fca9da34dd926
Gianlucagadotti <gianlucagadotti@gmail.com>
2025-04-24 22:16:44 -0300

    FEAT: #2 lista-livros

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,49 @@
 <script setup>
 
+import { ref, computed } from 'vue';
+
+const listaLivros = ref([
+  { nome: 'Chain of Iron: Volume 2',
+    imagem: '/public/image1.png',
+    autor: 'Cassandra Clare',
+ },
+  { nome: 'Chain of Thorns',
+   imagem: '/public/image2.png',
+   autor: 'Cassandra Clare',
+  },
+  { nome: 'City of Fallen Angels',
+    imagem: '/public/image3.png',
+    autor: 'Cassandra Clare',
+
+  },
+  { nome: 'Nona the Ninth',
+    imagem: '/public/image4.png',
+    autor: 'Cassandra Clare',
+
+  },
+  { nome: 'Harlem Shuffle',
+    imagem: '/public/image5.png',
+    autor: 'Colson Whitehead',
+
+  },
+  { nome: 'Two Old Women',
+    imagem: '/public/image6.png',
+    autor: 'Velma Wallis',
+
+  },
+  { nome: 'Carrie Soto Is Back',
+    imagem: '/public/image7.png',
+    autor: 'Taylor Jenkins Reid',
+
+  },
+  { nome: 'Book Lovers',
+    imagem: '/public/image8.png',
+    autor: 'Emily Henry',
+
+  }
+
+])
+
 </script>
 
 <template>
@@ -23,7 +67,23 @@
 
 </section>
 
+<section class="sessaoLivro">
+
+
+  <ul>
+    <li v-for="itens in listaLivros" class="abaLivros">
+
+      <img :src="itens.imagem" alt="">
+      <p>{{ itens.nome }}</p>
+      <p>{{ itens.autor }}</p>
+
+    </li>
+  </ul>
+
+</section>
 </main>
+
+
 
 </body>
 </template>
@@ -31,7 +91,7 @@
 <style scoped>
 
  /*//////////////Gianluca-livro em destaque////////////////////////////////////////////////////////////////*/
- 
+
   section.lancamentos{
     display: flex;
     font-size: 0.4rem;
@@ -52,7 +112,7 @@
   .lancamentos div p.verde{
     color: #27AE60;
     border: #27AE60 solid 1px;
-    width: 8vw;
+    width: 6vw;
     border-radius: 0.3vw;
     padding: 0.4vw 0 0.4vw 0.5vw;
     font-size: 0.5rem;
@@ -69,9 +129,27 @@
     border: none;
     border-radius: 0.2vw;
     padding: 1vw;
+    cursor: pointer;
   }
   .lancamentos h1{
     font-weight: bold;
     font-size: 1.5rem;
+  }
+  /*/////////////////////////////gianluca-lista_livros////////////////////////////////////*/
+
+ .sessaoLivro ul li img{
+    width: 15vw;
+  }
+ .sessaoLivro ul{
+    display: flex;
+    flex-wrap: wrap;
+    width: 80%;
+
+  margin: 0 auto;
+  list-style-type: none;
+
+  }
+  .sessaoLivro ul li{
+    padding: 0 1vw 1vw 1vw;
   }
 </style>
