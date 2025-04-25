090733865d8e3c8452598d7583a8f49f729f6dce
gustavoalvhnr <gustavoalves.ho@gmail.com>
2025-04-25 11:52:30 -0300

    FEAT 3# criação da div de icones

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
-<script setup>
-
-</script>
+<script setup></script>
 
 <template>
   <body class="corpo">
     <header>
-      <a href="#"><span class="conteudo1">IFBooks</span> <span class="conteudo2">Apreço a leitura</span></a>
-
+      <a href="#"
+        ><span class="conteudo1">IFBooks</span> <span class="conteudo2">Apreço a leitura</span></a
+      >
       <form>
         <input type="text" placeholder="Pesquisar" />
       </form>
@@ -27,33 +26,51 @@
           </li>
         </ul>
       </nav>
-        <ul>
-          <li>
-            <button class="icon"><font-awesome-icon icon="fa-solid fa-cart-shopping" /></button>
-          </li>
-          <li>
-            <button class="icon" id="coracao"><font-awesome-icon icon="fa-solid fa-heart" /></button>
-          </li>
-          <li>
-            <button class="icon"><font-awesome-icon icon="fa-solid fa-user" /></button>
-          </li>
-        </ul>
+      <ul>
+        <li>
+          <button class="icon"><font-awesome-icon icon="fa-solid fa-cart-shopping" /></button>
+        </li>
+        <li>
+          <button class="icon" id="coracao"><font-awesome-icon icon="fa-solid fa-heart" /></button>
+        </li>
+        <li>
+          <button class="icon"><font-awesome-icon icon="fa-solid fa-user" /></button>
+        </li>
+      </ul>
     </header>
     <main>
-    <section class="lancamentos">
-  <div>
-  <p class="verde">Autor de Abril</p>
+      <section class="lancamentos">
+        <div>
+          <p class="verde">Autor de Abril</p>
 
-  <h1>Eric-Emanuel Schmitt </h1>
+          <h1>Eric-Emanuel Schmitt</h1>
 
-  <p>Eric-Emmanuel Schmitt has been awarded more than 20 literary prizes and distinctions, and in 2001 he received the title of Chevalier des Arts et des Lettres. His books have been translated into over 40 languages.</p>
+          <p>
+            Eric-Emmanuel Schmitt has been awarded more than 20 literary prizes and distinctions,
+            and in 2001 he received the title of Chevalier des Arts et des Lettres. His books have
+            been translated into over 40 languages.
+          </p>
 
-  <button>Acessar página do livro</button>
-
-</div>
-  <img src="/public/Schmitt_Nocognia_3D_500pcx 1.png" alt="livro">
-
-</section>
+          <button>Acessar página do livro</button>
+        </div>
+        <img src="/public/Schmitt_Nocognia_3D_500pcx 1.png" alt="livro" />
+      </section>
+      <div class="icones">
+        <ul>
+          <li>
+            <font-awesome-icon icon="fa-solid fa-truck" />
+            <p>Frete grátis para SC</p>
+          </li>
+          <li class="estrela">
+            <font-awesome-icon icon="fa-solid fa-star" />
+            <p>Livros recomendados</p>
+          </li>
+          <li>
+            <font-awesome-icon icon="fa-solid fa-book-open" />
+            <p>Mais vendidos</p>
+          </li>
+        </ul>
+      </div>
     </main>
   </body>
 </template>
@@ -76,23 +93,23 @@ header {
 header nav ul {
   display: flex;
 }
-header ul{
+header ul {
   display: flex;
 }
-header a{
+header a {
   display: flex;
 }
-.conteudo1{
+.conteudo1 {
   color: #231f2d;
   font-weight: bolder;
   margin: 0.4vw 0.2vw 0 0;
 }
-.conteudo2{
+.conteudo2 {
   color: transparent;
   position: relative;
 }
-.conteudo2::before{
-  content: "apreço a \A leitura";
+.conteudo2::before {
+  content: 'apreço a \A leitura';
   color: #27ae60;
   border-left: #27ae60 solid 2px;
   padding-left: 0.156vw;
@@ -101,7 +118,7 @@ header a{
   left: 0;
   top: 0;
 }
-header form input{
+header form input {
   color: #b8b8b8;
   background-color: #f1f1f1;
   border: none;
@@ -114,71 +131,92 @@ header form input{
   padding: 0 0 0 1vw;
   margin: 0.2vw 0 0 0;
 }
-header button{
+header button {
   border: none;
   background-color: rgba(0, 0, 0, 0);
   margin: 0.4vw;
   color: #7b7881;
   font-size: 1.1rem;
 }
-.nave{
+.nave {
   padding: 0 1vw;
 }
-.icon{
+.icon {
   color: #27ae60;
   padding: 0 1vw 0 1vw;
 }
-#coracao{
+#coracao {
   border-left: #27ae60 solid 2px;
   border-right: #27ae60 solid 2px;
 }
-header nav button{
+header nav button {
   padding: 0 0.5vw;
 }
 
- /*//////////////Gianluca-livro em destaque////////////////////////////////////////////////////////////////*/
+/*//////////////Gianluca-livro em destaque////////////////////////////////////////////////////////////////*/
 
- section.lancamentos{
-    display: flex;
-    font-size: 0.4rem;
+section.lancamentos {
+  display: flex;
+  font-size: 0.4rem;
+}
 
-  }
-
-  .lancamentos div{
-    margin: 8vw 8vw 8vw 20vw;
-
-  }
-  .lancamentos p{
-    width: 29vw;
-    padding: 1vw 0;
-    font-size: 1.3rem;
-    margin-bottom: 1vw;
-
-  }
-  .lancamentos div p.verde{
-    color: #27AE60;
-    border: #27AE60 solid 1px;
-    width: 8.5vw;
-    border-radius: 0.3vw;
-    padding: 0.4vw 0 0.4vw 0.5vw;
-    font-size: 1.3rem;
-
-
-  }
-  .lancamentos img{
-    width: 29vw;
-  }
-  .lancamentos button{
-    font-size: 1.3rem;
-    color: white;
-    background-color: #27AE60;
-    border: none;
-    border-radius: 0.2vw;
-    padding: 1vw;
-  }
-  .lancamentos h1{
-    font-weight: bold;
-    font-size: 3rem;
-    padding: 1vw 0 1vw 0;
-  }
+.lancamentos div {
+  margin: 8vw 8vw 8vw 20vw;
+}
+.lancamentos p {
+  width: 29vw;
+  padding: 1vw 0;
+  font-size: 1.3rem;
+  margin-bottom: 1vw;
+}
+.lancamentos div p.verde {
+  color: #27ae60;
+  border: #27ae60 solid 1px;
+  width: 8.5vw;
+  border-radius: 0.3vw;
+  padding: 0.4vw 0 0.4vw 0.5vw;
+  font-size: 1.3rem;
+}
+.lancamentos img {
+  width: 29vw;
+}
+.lancamentos button {
+  font-size: 1.3rem;
+  color: white;
+  background-color: #27ae60;
+  border: none;
+  border-radius: 0.2vw;
+  padding: 1vw;
+}
+.lancamentos h1 {
+  font-weight: bold;
+  font-size: 3rem;
+  padding: 1vw 0 1vw 0;
+}
+/*//////////////////////////
+    Div.icones (Gustavo)
+//////////////////////////*/
+.icones {
+  border-bottom: #27ae60 2px solid;
+  border-top: #27ae60 2px solid;
+  padding: 3.5vw;
+  color: black;
+}
+.icones ul {
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  font-size: 2rem;
+}
+.icones ul li{
+  display: flex;
+}
+.icones ul li.estrela{
+  border-left: #27ae60 2px solid;
+  border-right: #27ae60 2px solid;
+  padding: 0 3vw 0 3vw;
+}
+.icones ul li p{
+  padding: 0 2vw;
+}
 </style>
